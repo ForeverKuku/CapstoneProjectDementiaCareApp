f2c7b78430016733280d7d67dbf8aaa050ad3e57
kellen-ira <ikellen2016@gmail.com>
2024-04-09 13:25:57 +0200

    update on profile

--- src/navigation.jsx
+++ src/navigation.jsx
@@ -9,7 +9,7 @@ import WelcomeScreen from './onboardingScreens/WelcomeScreen';
 import CaregiverDashboard from './CareGiverScreens/CaregiverDashboard';
 import PatientDashboard from './PatientScreens/PatientDashboard';
 import EmergencyCall from './PatientScreens/EmergencyCall';
-import ProfileScreen from './PatientScreens/ProfileScreen';
+import PatientProfile from './PatientScreens/PatientProfile';
 import Feather from 'react-native-vector-icons/Feather';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 
@@ -27,7 +27,7 @@ export const MainNavigation = () => {
      <Stack.Screen name="WelcomeScreen" component={WelcomeScreen}  options={{headerShown: false}}/>
      <Stack.Screen name="CaregiverDashboard" component={CaregiverDashboard}  options={{headerShown: false}}/>
      <Stack.Screen name="EmergencyCall" component={TabNavigator}  options={{headerShown: false}}/>   
-    <Stack.Screen name="ProfileScreen" component={TabNavigator}  options={{headerShown: false}}/> 
+    <Stack.Screen name="PatientProfile" component={TabNavigator}  options={{headerShown: false}}/> 
     <Tab.Screen name="PatientDashboard" component={TabNavigator}  options={{tabBarShowLabel:false}} /> 
      </Stack.Navigator>
  )
@@ -47,7 +47,7 @@ function TabNavigator() {
             iconName = 'home';
           } else if (route.name === 'EmergencyCall') {
             iconName = 'phone-call';
-          }   else if (route.name === 'ProfileScreen') {
+          }   else if (route.name === 'PatientProfile') {
             iconName = 'user';
           }
           
@@ -65,7 +65,7 @@ function TabNavigator() {
   >
     <Tab.Screen name="PatientHomeScreen" component={PatientDashboard}  options={{tabBarShowLabel:false}}/>
     <Tab.Screen name="EmergencyCall" component={EmergencyCall}  options={{tabBarShowLabel:false}} />
-    <Tab.Screen name="ProfileScreen" component={ProfileScreen}  options={{tabBarShowLabel:false}} />  
+    <Tab.Screen name="PatientProfile" component={PatientProfile}  options={{tabBarShowLabel:false}} /> 
   </Tab.Navigator>
   );
 }
